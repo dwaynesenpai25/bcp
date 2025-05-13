14de125fbb7dafe1c4b503fb2474a665b4f6107b
dwaynesenpai25 <xxstopxx3@gmail.com>
2025-05-13 17:02:46 +0800

    Update BCP automation logs and SQL query for active debtors

--- query/fetch_active.sql
+++ query/fetch_active.sql
@@ -6,3 +6,4 @@ LEFT JOIN `client` ON client.id = debtor.client_id
 WHERE client.id IN ({selected_client_id})
     AND debtor.is_aborted <> 1
     AND debtor.is_locked <> 1
+    AND debtor.deleted_at IS NULL
